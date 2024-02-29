aee43bf63071833b322b683e1ee30201371a6071
Chloe Yuan <156484403+Chloemh48@users.noreply.github.com>
2024-02-29 00:55:07 -0800

    Version3
    Extract the daily max_temperature from March 2023 to Feb 2024, Then calculate the monthly average temperature

--- data_visulize.py
+++ data_visulize.py
@@ -2,6 +2,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import json
+from datetime import datetime
+from collections import defaultdict
 
 # x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
 # y = [38496, 42000, 46752, 49320, 53200, 56000, 62316, 64928,
@@ -14,25 +16,46 @@ import json
 # plt.show()
 
 
-def visulize():
-    data_temps = []
+def visualize():
     with open('/Users/liyuanyuan/Desktop/GitHub/wp_4/chino.txt', 'r') as file:
-        print("open file")
-        data = json.load(file) 
+        print("Open file")
+        data = json.load(file)  
 
+
+    data_temps = []
     for tp in data["data"]:
+        
         data_temps.append([tp["datetime"], tp["max_temp"]])
 
     with open('temp.txt', 'w') as f:
-            
+        json.dump(data_temps, f, indent=4)
 
-        json.dump(data_temps, f, indent= 4)
+    monthly_temps = defaultdict(lambda: {'sum': 0, 'count': 0})
+
+
+    for date_str, temp in data_temps:
+        date_obj = datetime.strptime(date_str, "%Y-%m-%d")
+        month_year = date_obj.strftime('%Y-%m')  # Format as 'YYYY-MM'
+        
+        # Process dates from March 2023 to February 2024
+        if datetime(2023, 3, 1) <= date_obj <= datetime(2024, 2, 29):
+            monthly_temps[month_year]['sum'] += temp
+            monthly_temps[month_year]['count'] += 1
+
+    # Calculate average temperatures per month
+    avg_temp = []
+    for month_year, temps in sorted(monthly_temps.items()):
+        average_temp = temps['sum'] / temps['count']
+        print(f"Average temperature for {month_year}: {average_temp:.2f}°")
+        avg_temp. append(f'{average_temp:.2f}°')
+    print(avg_temp)
+
 
 
 
 def main():
     
-    visulize()
+    visualize()
 
 if __name__ == '__main__':
     main()
