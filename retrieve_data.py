0957aa89a4b052c206eaf26632e0ffba4358e65d
Chloe Yuan <156484403+Chloemh48@users.noreply.github.com>
2024-03-04 01:13:21 -0800

    Add I/O module to deal with the file
    To fulfill the requirement, I added the data_into_file module to handle the file operations.

--- retrieve_data.py
+++ retrieve_data.py
@@ -2,8 +2,7 @@
 import urllib.request
 import json
 import ssl
-from datetime import datetime
-from collections import defaultdict
+
 
 def get_air_quality():
     
@@ -28,48 +27,4 @@ def get_air_quality():
     with open(filename_out, 'w') as file_out:
         json.dump(api_data_obj, file_out)
     return filename_out
-
-
-def cal_mean_aqi(filename_out):
-    print('Data is loading...')
-    try:
-        with open(filename_out,'r') as file:
-
-            print("File is openning...")
-            data = json.load(file)  
-    except Exception as e:
-        print(f'Error: {e}')
-        return
-
-    # aqi stands for air quality index value
-    data_aqi = []
-    for aqi in data["data"]:
-        
-        data_aqi.append([aqi["aqi"],aqi['datetime']])
-
-    with open('aqi.txt', 'w') as f:
-        json.dump(data_aqi, f, indent=4)
-
-    daily_aqi = defaultdict(lambda: {'sum': 0, 'count': 0})
-
-
-    for item in data_aqi:
-        value, date_str = item[0], item[1] 
-        date_obj = datetime.strptime(date_str.split(':')[0], "%Y-%m-%d")
-        daily = date_obj.strftime('%Y-%m-%d')  
-        
-        # Process dates from 2023-01-01 to  2023-01-08
-        if datetime(2024, 2, 1) <= date_obj <= datetime(2024, 2, 29):
-            daily_aqi[daily]['sum'] += value
-            daily_aqi[daily]['count'] += 1
-
-    # Calculate average aqi per day
-    avg_aqi = []
-    for daily, value in sorted(daily_aqi.items()):
-        average_aqi = value['sum'] / value['count']
-        print(f"Mean aqi for {daily}: {average_aqi:.2f}°")
-        avg_aqi.append(f'{average_aqi:.2f}°')
-  
-    return avg_aqi
-
 
--- wp4.py
+++ wp4.py
@@ -1,13 +1,13 @@
 
 import retrieve_data as rd
 import data_visulize as dv
+import date_into_file as d
 
 
 def main():
     filename_out = rd.get_air_quality()
-
     if filename_out:
-        avg_aqi = rd.cal_mean_aqi(filename_out)
+        avg_aqi = d.cal_mean_aqi (filename_out)
         print(avg_aqi)
         
         dv.plot(avg_aqi)
